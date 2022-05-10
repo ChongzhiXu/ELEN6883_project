7286e9864f7cea85fbc73216d7da28c60a063aa4
CHONGZHI XU <90977332+ChongzhiXu@users.noreply.github.com>
2022-05-10 13:20:07 -0400

    update some backend stuff

--- Final_project/marketPlace/server.py
+++ Final_project/marketPlace/server.py
@@ -3,260 +3,119 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 import time
-# DATA
+import pymongo
+from pymongo import MongoClient
 
-learning_data = {
+
+# Music items database
+items = {
     "1": { 
     "id": "1", 
-    "page_title": "Introduction",
-    "info1": ["What is a distress signal?", "A distress signal, also known as a distress call, is an internationally recognized way of indicating that you are in serious and/or immediate danger and are in need of help."],
-    "info2": [ "","You should not use a distress signal if you are not in serious and/or immediate danger."],
-    "info3":[ "",""],
-    "media": ["https://aceboater.com/media/guide/1/visual-distress-signals.jpg","https://pbs.twimg.com/media/DPPj9YeXcAI2P8D.jpg"],
-    "category": "introduction",
-    "subcategory": ""
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "2": { 
     "id": "2", 
-    "page_title": "Maritime Distress Signals",
-    "info1": ["","We will first go over maritime boat signals, that is, distress signals that ships at sea use to communicate need for help."],
-    "info2": [ "","The types of signals are divided into three categories: visual, sound, and radio."],
-    "info3": [ "",""],
-   "media":["https://assets.kalkomey.com/boaterexam/images/fm/boatingresources/visual-distress-signals.gif"],
-    "category": "maritime",
-    "subcategory": "intro"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "3": { 
     "id": "3", 
-    "page_title": "Visual Maritime Distress Signals",
-    "info1": ["What are common visual distress signals?", ["Red flares/sparks","Orange Smoke","Sea Marker Dye","Orange Distress Flag","Waving arms"]],
-    "info2": [ "What is the benefit of visual distress signals?","The benefit of visual distress signals is that they not only draw attention to a ship’s distress from a distance but they help indicate the location of the troubled party.","Note: it is required by federal law for ships sailing on coastal waters to carry visual distress signals."],
-    "info3": ["","Note: it is required by federal law for ships sailing on coastal waters to carry visual distress signals."],
-    "media":["https://ke-courses-production.s3.amazonaws.com/asset_files/production/1559/attachments/original/distress_signals_set.jpg?1503937958"],
-    "category": "maritime",
-    "subcategory": "visual"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "4": { 
     "id": "4", 
-    "page_title": "Sound Maritime Distress Signals",
-    "info1": ["What are common sound maritime distress signals?", ["Firing gun or rocket at regular intervals","Making a continuous sound with a fog-signaling apparatus (aka foghorn), bell, or whistle","Yelling for help (usually if there are no other options)"]],
-    "info2": [ "What are the benefits of sound maritime distress signals?","They grab the attention of local vessels or settlements"],
-    "info3": [ "",""],
-    "media": ["https://www.plastimo.com/media/catalog/product/cache/2/image/1200x/040ec09b1e35df139433887a97daa66f/p/l/pls_trompe-.jpg","https://m.media-amazon.com/images/I/81InNZC9uDL.jpg"],
-    "category": "maritime",
-    "subcategory": "sound"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "5": { 
     "id": "5", 
-    "page_title": "Maritime Radio Distress Signals",
-    "info1": ["How do you communicate distress through the radio?", ["Transmit a spoken Mayday message ('Mayday, Mayday, Mayday. This is [boat name/call sign]. x3')", "Send out an SOS message by Morse Code "]],
-    "info2": [ "","Fun fact: Mayday comes from the English pronunciation of the French phrase for help “m’aidez”"],
-    "info3": [ "",""],
-    "media": ["https://i0.wp.com/www.malpope.com/wp-content/uploads/2021/05/ba665b97967605506331d3bc0c012ab7-e1619862114772.jpg?resize=850%2C667&ssl=1","https://pbs.twimg.com/media/DPPj9YeXcAI2P8D.jpg"],
-    "category": "maritime",
-    "subcategory": "radio"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "6": { 
     "id": "6", 
-    "page_title": "Maritime Distress Signals Review",
-    "info1": ["Check out this short video to review what you’ve learned!", ""],
-    "info2": [ "",""],
-    "info3": [ "",""],
-    "media": ["https://www.youtube.com/embed/zjVJKa7faW8"],
-    "category": "maritime",
-    "subcategory": "video"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
-    "7": {
-        "id": "7",
-        "page_title": "Maritime Distress Signals Mini Quiz",
-        "question": "Which of the following is not a common maritime distress signal?",
-        "a": "Orange smoke",
-        "b": "Bonfire",
-        "c": "Whistle",
-        "d": "MayDay",
-        "answer": "b",
-        "feedback": "Orange Smoke is a common visual signal, Whistle is a common sound signal, and MayDay is a common radio signal. It is not recommended to build a bonfire on your boat.",
-        "category": "maritime",
-        "subcategory": "quiz",
-    },    
+    "7": { 
+    "id": "7", 
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
+    },
     "8": { 
     "id": "8", 
-    "page_title": "Wilderness Distress Signals",
-    "info1": ["", "This group of distress signals applies if, for example, you are stranded on a deserted island or lost in the wilderness. These solution assume you do not have proper equipment like a satellite phone, flare, or whistle to call for help."],
-    "info2": [ "",""],
-    "info3": [ "",""],
-    "media": ["https://media.30seconds.com/tip/lg/Youre-About-to-Be-Stranded-on-a-Deserted-Island-What-4-Ite-15490-8661b483e3-1516247705.jpg"],
-    "category": "wilderness",
-    "subcategory": "intro"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "9": { 
     "id": "9", 
-    "page_title": "Reflective Surfaces",
-    "info1": ["", "Reflective surfaces can be used to signal airplanes flying up above. A signal mirror is the preferred tool but anything reflective like a credit card or belt buckle will work."],
-    "info2": [ "",""],
-    "info3": [ "",""],
-    "media": ["https://www.survivalresources.com/images/banner/PageHeader.Original.jpg"],
-    "category": "wilderness",
-    "subcategory": "reflective"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "10": { 
     "id": "10", 
-    "page_title": "Signal Fires",
-    "info1": ["Why light a signal fire?", "A signal fire is useful because it can be see for miles."],
-    "info2": [ "What should a signal fire look like?",["One or Three Signal Fires in a Triangle Formation in an open space","(Depending on Resources) First layer dry tinder, second layer wood kindling, third layer moss and decaying plant life (optional), final layer green leafy vegetation"]],
-    "info3":[ "",""],
-    "media": ["https://arestlessart.files.wordpress.com/2022/02/signal-fires.jpg"],
-    "category": "wilderness",
-    "subcategory": "signal_fire"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
     "11": { 
     "id": "11", 
-    "page_title": "Signal Fires",
-    "info1": ["Watch this video to see a signal fire being built!",""],
-    "info2": [ "",""],
-    "info3":[ "",""],
-    "media": ["https://www.youtube.com/embed/0G857JwMsM8"],
-    "category": "wilderness",
-    "subcategory": "video"
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
     },
-    "12": {
-        "id": "12",
-        "category": "wilderness",
-        "subcategory": "signal_fire_activity"
-    },
-    "13": {
-        "id": "13",
-        "page_title": "Wilderness Distress Signals Mini Quiz",
-        "question": "Which of the following cannot be used as a signal mirror?",
-        "a": "Belt Buckle",
-        "b": "Mirror",
-        "c": "Rock",
-        "d": "Credit Card",
-        "answer": "c",
-        "feedback": "All of the other options are reflective in some way and therefore can be used as a signal mirror to communicate distress.",
-        "category": "wilderness",
-        "subcategory": "quiz",
-    },      
-     "14": { 
-    "id": "14", 
-    "page_title": "Street Distress Signals",
-    "info1": ["", "Aside from being lost at sea or stranded on a deserted island, you may have other reasons to signal for distress. And though most people seem to have easy access to a phone at all times, there are scenarios when calling for help may not be possible."],
-    "info2": [ "In what specific scenarios may one need to call for help but be unable to?","Domestic Violence, Human Trafficking, and Kidnapping"],
-    "info3": [ "What should I do if I find myself in one of these scenarios and can’t call for help?","Do the international hand signal for help"],
-    "media": ["https://www.verywellhealth.com/thmb/WNrtvId6QPMIaIYGnnEPXn6rqfk=/614x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Tiktokhandsignalcloseup-FINAL-46d537b506fb484eb69b851c8acbf772.jpg"],
-    "category": "streets",
-    "subcategory": "intro"
-    },
-     "15": { 
-    "id": "15", 
-    "page_title": "Street Distress Signals",
-    "info1": ["", ""],
-    "info2": [ "",""],
-    "info3": [ "",""],
-    "media": ["https://www.youtube.com/embed/AFLZEQFIm7k"],
-    "category": "streets",
-    "subcategory": "video"
-    },
-    "16": {
-        "id": "16",
-        "page_title": "Street Distress Signals Mini Quiz",
-        "question": "Which of the following is not a reason to use the international hand signal for help?",
-        "a": "Domestic violence",
-        "b": "Human Trafficking",
-        "c": "Kidnapping",
-        "d": "Fire",
-        "answer": "d",
-        "feedback": "If there is a fire, call 911.",
-        "category": "streets",
-        "subcategory": "quiz",
-    },  "17": {
-        "id": "17",
-        "category": "xx",
-        "subcategory": "xx",
-    },  
-};
-
-fire_simulation = {
-    "1": { 
-    "ordernum": "1", 
-    "name":"Dry Tinder",
-    "image":"https://b.rgbimg.com/users/l/lu/lusi/600/mgyuCEy.jpg"
-    },
-    "2": { 
-    "ordernum": "3", 
-    "name":"Moss/Decaying Plants",
-    "image":"https://cdn.shopify.com/s/files/1/0260/8191/9066/products/Chartreuse_580x.png?v=1629396828"
-    },
-    "3": { 
-    "ordernum": "4", 
-    "name":"Leafy Vegetation",
-    "image":"https://m.media-amazon.com/images/I/81BHFiAx4BL._AC_SX425_.jpg"
-    },
-     "4": { 
-    "ordernum": "2", 
-    "name":"Wood Kindling",
-    "image":"https://m.media-amazon.com/images/I/513Xz4L6SHL._AC_US200_.jpg"
-    } 
-};
-
-test_data = {
-    "1": {"id": "1",
-          "question": "Which of the following is not a category of maritime distress signal?",
-          "a": "Visual",
-          "b": "Sound",
-          "c": "Olfactory",
-          "d": "Radio"},
-    "2": {"id": "2", "question": "True or False? You separate your first two and second two fingers to indicate distress.",
-          "a": "True",
-          "b": "False"},
-    "3": {"id": "3",
-          "question": "Which of the following is not ideal fuel for your signal fire?",
-          "a": "Moss",
-          "b": "Twig",
-          "c": "Wet Wood",
-          "d": "Leafy Vegetation"},
-    "4": {"id": "4",
-          "question": "How many times (at minimum) should you repeat “Mayday” when sending a distress signal over the radio?",
-          "a": "1",
-          "b": "2",
-          "c": "3",
-          "d": "4"},
-    "5": {"id": "5",
-          "question": "Which of the following is not a common visual maritime distress signal?",
-          "a": "Orange sea dye",
-          "b": "Red sea dye",
-          "c": "Orange Flag",
-          "d": "Red sparks"},
+    "12": { 
+    "id": "12", 
+    "item_title": "",
+    "item_info": "",
+    "item_media": "",
+    "item_image": ""
+    }
 }
 
-test_answers = {
-    "1": "c",
-    "2": "b",
-    "3": "c",
-    "4": "c",
-    "5": "b",
-    }
+# MongoDB data service
+def mongoFind(acconut_id):
+    mongodb_url = "mongodb+srv://mongodb:Xcz990208@cluster0.rfss2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongo_client = MongoClient(mongodb_url)
+    db = mongo_client["user_Database"]
+    Users = db["Users"]
 
-# fill in the rest of quiz info
-feedback = {
-    "1": "You typically do not signal other vessels using scents.",
-    "2": "That is the Vulcan salute. To indicate distress, you fold your fingers over your thumb in a fist.",
-    "3": "Wet wood is harder to light and harder to keep burning.",
-    "4": "The recommended format for sending out a Mayday distress signal is “Mayday, Mayday, Mayday. This is [boat name/call sign].”",
-    "5": "Orange sea dye is a common visual maritime distress signal, red sea dye is not.",
-    }
+def mongoPost(account_id):
+    mongodb_url = "mongodb+srv://mongodb:Xcz990208@cluster0.rfss2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongo_client = MongoClient(mongodb_url)
+    db = mongo_client["user_Database"]
+    Users = db["Users"]
 
-activity = {}
-attempt_cnt = 0
-test_record = {}
-user_ans = ""
+def mongoUpdate(account_id):
+    mongodb_url = "mongodb+srv://mongodb:Xcz990208@cluster0.rfss2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongo_client = MongoClient(mongodb_url)
+    db = mongo_client["user_Database"]
+    Users = db["Users"]
 
-score = 0
-
-# current_id = 12;
-
-# ROUTES
-
+    
+# Server process route
 @app.route('/')
 def welcome():
    global activity
