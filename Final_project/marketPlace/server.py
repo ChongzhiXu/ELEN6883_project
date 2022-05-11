1278b405b16fde4b01fb0ee4c9a7e6ef05aacc46
CHONGZHI XU <90977332+ChongzhiXu@users.noreply.github.com>
2022-05-10 20:08:33 -0400

    Update server.py

--- Final_project/marketPlace/server.py
+++ Final_project/marketPlace/server.py
@@ -145,6 +145,13 @@ def display():
    global items
    return render_template('shop_grid.html', items=items)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login_check():
+    user_info = request.get_json()
+    if user_info['user_name'] in ['ChongzhiXu'] and user_info['password'] in ['12345678']:
+        return render_template('shop_grid.html')
+    else:
+        return render_template('home_page.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
