a4753746f0c75ada2fc0ae06ff8b5b0f2a7114ba
CHONGZHI XU <90977332+ChongzhiXu@users.noreply.github.com>
2022-05-11 03:06:03 -0400

    Update home_page.js

--- Final_project/marketPlace/static/home_page.js
+++ Final_project/marketPlace/static/home_page.js
@@ -1,23 +1,36 @@
-$(document).ready(function () {
-
+function submit_signin_Form(){
     $("#sign_in_form").submit(function(event) {
         
         event.preventDefault()
         console.log("submit");
 
         var user_login = {
-            "user_name": $("#username_input").val().trim(),
-            "password": $("#password_input").val().trim()
+            "user_name": $("#input-usename").val().trim(),
+            "password": $("#input-password").val().trim()
         }
 
         $.ajax({
             type: "POST",
-            url: "/login",
+            url: "login",
             dataType : "json",
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify(user_login),
-            success: function () {
-                console.log("post successfully!");
+            success: function (response) {
+                console.log("Post successfully!");
+                if (response["url"] == 'shop_items'){
+                    $('.alert').show()
+                    $("#success-notification").empty()
+                    $("#success-notification").html("Login Successfully! <a href='" + response["url"] + "'>Go to the Market here!<\a>")
+                    $(".form-control").val("")
+                    $("#input-name").focus();
+                    $('#sign_in_form').hide()
+                } else {
+                    $('.alert').show()
+                    $("#success-notification").empty()
+                    $("#success-notification").html("Login Failed, please check your username and password.")
+                    $(".form-control").val("")
+                    $("#input-name").focus();
+                }
             },
             error: function(request, status, error){
                 console.log("Error");
@@ -27,4 +40,64 @@ $(document).ready(function () {
             }
         }); 
     });
+}
+
+function submit_signup_Form(){
+    $("#sign_up_form").submit(function(event) {
+        
+        event.preventDefault()
+        console.log("submit");
+
+        var user_register = {
+            "user_name": $("#input-usename-up").val().trim(),
+            "password": $("#input-password-up").val().trim(),
+            "email": $("#input-email").val().trim(),
+            "ale_id": $("#input-aleid").val().trim()
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "register",
+            dataType : "json",
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(user_register),
+            success: function (response) {
+                console.log("Post successfully!");
+                if (response["url"] == '/'){
+                    $('.alert').show()
+                    $("#success-notification").empty()
+                    $("#success-notification").html("Sign up Successfully! <a href='" + response["url"] + "'>Sign in to access the Market!<\a>")
+                    $(".form-control").val("")
+                    $("#input-name-up").focus();
+                    $('#sign_up_form').hide()
+                } else {
+                    console.log('impossible')
+                }
+            },
+            error: function(request, status, error){
+                console.log("Error");
+                console.log(request)
+                console.log(status)
+                console.log(error)
+            }
+        }); 
+    });
+}
+
+function show_signup(){
+    $('#sign_in_form').hide()
+    $('#sign_up_form').show()
+}
+
+function show_signin(){
+    $('#sign_up_form').hide()
+    $('#sign_in_form').show()
+}
+
+
+$(document).ready(function () {
+
+    $('.alert').hide()
+    $('#sign_up_form').hide()
+
 });
